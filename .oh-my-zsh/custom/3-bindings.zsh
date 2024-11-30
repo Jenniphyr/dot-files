208edf13abc02c23c95c034598913d9c55d9c823
Jennifer Narzt <jennifer.narzt@gmail.com>
2024-11-29 23:17:20 -0700

    more bash bindings

--- .oh-my-zsh/custom/3-bindings.zsh
+++ .oh-my-zsh/custom/3-bindings.zsh
@@ -3,3 +3,7 @@ bindkey 'b' emacs-backward-word
 bindkey 'f' emacs-forward-word
 bindkey '^U' backward-kill-line
 bindkey '^W' kill-word
+
+# https://stackoverflow.com/questions/10847255/how-to-make-zsh-forward-word-behaviour-same-as-in-bash-emacs
+autoload -U select-word-style
+select-word-style bash
